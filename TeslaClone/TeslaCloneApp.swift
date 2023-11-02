b171e9cf4334afae3f56065289883dadfa126d9a
chhamilton000 <106551242+chhamilton000@users.noreply.github.com>
2023-11-02 13:00:59 -0500

    Initial Commit

---
+++ TeslaClone/TeslaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaCloneApp.swift
+//  TeslaClone
+//
+//  Created by Caley Hamilton on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TeslaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
